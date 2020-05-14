d8d43bed976fe01bbd29b5082ccf33326d4675c7
Patrick Furtak <patrick@patrickfurtak.com>
2020-05-14 01:12:55 -0400

    initial commit, boiler plate cleanup

---
+++ gatsby-config.js
@@ -0,0 +1,69 @@
+module.exports = {
+  siteMetadata: {
+    title: `PFurtak.Dev`,
+    description: `Full-stack software engineer focused on web applications. Always on the lookout for the next cool dev tool.`,
+    author: `Patrick Furtak`,
+    siteUrl: `https://www.pfurtak.dev`,
+    social: {
+      twitter: ``,
+      facebook: `patrick.furtak`,
+      github: `PFurtak`,
+      linkedin: `pfurtak`,
+      email: `patrick@patrickfurtak.com`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 970,
+            },
+          },
+          `gatsby-remark-prismjs`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `flexible-gatsby-starter`,
+        short_name: `flexible-gatsby`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `./static/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+  ],
+}
